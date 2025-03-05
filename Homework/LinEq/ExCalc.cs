52b87bb9b32908cc22447c2cdf8abcb14424f5f6
emillf <emillysholtfabergmail.com>
2025-03-05 11:26:49 +0100

    added partially completed LinAlg class

---
+++ Homework/LinEq/ExCalc.cs
@@ -0,0 +1,11 @@
+using System;
+using Sytem.Math;
+using vector;
+using matrix;
+public class Program{
+	var rnd new Random(3);
+	double x = rnd.NextDouble();
+	double y = rnd.NextDouble();
+	double z = rnd.NextDouble();
+	static void Main(){
+
