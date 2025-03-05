c2343e63ab4d948805987ac436a865a52a5cd784
emillf <emillysholtfabergmail.com>
2025-03-05 12:40:28 +0100

    Added Dmitri's matrix and vector classes and moved QR to them

--- Homework/LinEq/main.cs
+++ Homework/LinEq/main.cs
@@ -1,11 +1,9 @@
 using System;
 using Sytem.Math;
-using vector;
-using matrix;
 public class Program{
 	var rnd new Random(3);
 	double x = rnd.NextDouble();
 	double y = rnd.NextDouble();
 	double z = rnd.NextDouble();
 	static void Main(){
-
+		
