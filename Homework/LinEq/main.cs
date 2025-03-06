d153e1c61741c422f32fdbc06b16646387b9bbca
emillf <emillysholtfabergmail.com>
2025-03-06 12:58:11 +0100

    Added matrix.random to matrix.cs and continued work on main.cs

--- Homework/LinEq/main.cs
+++ Homework/LinEq/main.cs
@@ -1,9 +1,23 @@
 using System;
-using Sytem.Math;
+using static System.Console;
 public class Program{
-	var rnd new Random(3);
-	double x = rnd.NextDouble();
-	double y = rnd.NextDouble();
-	double z = rnd.NextDouble();
 	static void Main(){
-		
+        	var rnd = new Random(3);
+        	double x = rnd.NextDouble();
+        	double y = rnd.NextDouble();
+        	double z = rnd.NextDouble();
+		var A = matrix.random(7,3,rnd);
+		A.print("Random matrix A");
+		WriteLine("/n A decomposed into Q and R gives:/n");
+		QR = new QR;
+		var QRfact = QR.decomp(A);
+		var Q = QRfact.Item1;
+		var R = QRfact.Item2;
+		WriteLine("Q = /n");
+		Q.print;
+		WriteLine("R = /n");
+		R.print;
+		bool AeqQR=A.approx(QRfact);
+		WriteLine($"QR = A? {AeqQR}/n");
+		}
+	}
