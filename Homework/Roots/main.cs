1d5130ea0e8b82106c733d79f47c48205504f0a2
emillf <emillysholtfabergmail.com>
2025-04-20 20:53:08 +0200

    finished part a) missing proper xstart conditions

--- Homework/Roots/main.cs
+++ Homework/Roots/main.cs
@@ -3,8 +3,7 @@ using System.IO;
 using static System.Math;
 using static System.Console;
 public class Program{
-	static int Main(){
-		public static matrix jacobian(Func<vector,vector> f,vector x,vector fx=null,vector dx=null){
+		static matrix jacobian(Func<vector,vector> f,vector x,vector fx=null,vector dx=null){
 			if(dx == null) dx = x.map(xi => Abs(xi)*Pow(2,-26));
 			if(fx == null) fx = f(x);
 			matrix J=new matrix(x.size);
@@ -30,6 +29,7 @@ public class Program{
 				var QRJ = matrix.QR.decomp(J);
 				vector Dx = matrix.QR.solve(QRJ.Item1,QRJ.Item2,-fx); /* Newton's step */
 				double λ=1;
+				double λmin =Pow(10,-5);
 				do{ /* linesearch */
 					z=x+λ*Dx;
 					fz=f(z);
@@ -41,12 +41,18 @@ public class Program{
 				}while(true);
 			return x;
 			}
-		Func<vector,vector> Rosengrad = vector x => new vector y(-2*(1-x[0])-100*2*(x[1]-x[0]*x[0])*2*x[0],2*100*(x[1]-x[0]*x[0]));
-		Func<vector, vector> Himmelgrad = vector x => new vector y(2*(x[0]*x[0]+x[1]-11)*2*x[0]+2*(x[0]+x[1]-7),2*(x[0]*x[0]+x[1]-11)+2*(x[0]+x[1]*x[1]-7)*2*x[1]);
-		var eksHimmel = newton(Himmelgrad);
-		var eksRosen = newton(Rosengrad);
-		eksHimmel.Print();
-		eksRosen.Print();
-		WriteLine($"{}\n");
+		static int Main(){
+		var xstarthimmel = new vector(10,10);
+		Func<vector,vector> test = x => new vector(3*x[0]*x[0],3*x[1]*x[1]);
+		Func<vector,vector> Rosengrad = x => new vector(-2*(1-x[0])-100*2*(x[1]-x[0]*x[0])*2*x[0],2*100*(x[1]-x[0]*x[0]));
+		Func<vector, vector> Himmelgrad = x => new vector(2*(x[0]*x[0]+x[1]-11)*2*x[0]+2*(x[0]+x[1]-7),2*(x[0]*x[0]+x[1]-11)+2*(x[0]+x[1]*x[1]-7)*2*x[1]);
+		var eksHimmel = newton(Himmelgrad,xstarthimmel);
+		var testres = newton(test,xstarthimmel);
+		var eksRosen = newton(Rosengrad,xstarthimmel);
+		eksHimmel.print();
+		testres.print();
+		eksRosen.print();
+		WriteLine($"{1}\n");
+		return 1;
 		}//Main
 	}//Program
